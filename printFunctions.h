4198f39b25fbc106516a4bfd29fbdf7e666abdb1
Christopher Lee <leechris.main@gmail.com>
2012-05-14 13:00:38 -0500

    Russell's bugfixes

--- printFunctions.h
+++ printFunctions.h
@@ -7,30 +7,17 @@
 #include <time.h>
 #include "dataStructures.h"
 #include "calEnum.h"
-
-
-
-char * substr(char* str, int start, int length);
-
-void fprintTimes(FILE* html);
-
-void fprintWeekDay(int d,FILE* html);
-
-void printEventNode(event_node *event,FILE* html);
-
-void printEmptyEventNode(FILE* html);
+void printEventNode(event_node *event);
 
 void printColor(enum color c);
 
 void printFont(enum font c);
 
-void printEvents(event_node *firstEvent,FILE* html);
+void printEvents(event_node *firstEvent);
 
 
 void printConfigurationInfo(configuration_info * configInfo);
 void printWeekDay(enum day_of_week d);
 
-
-FILE* printHeader();
 
 #endif
